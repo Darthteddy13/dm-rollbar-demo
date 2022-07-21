654a4ed7a40b65be1d921175551295f1da2b2994
Kevin Taylor <kevintay96@gmail.com>
2022-07-21 12:37:11 -0600

    added rollbar logs

--- server.js
+++ server.js
@@ -19,11 +19,12 @@ rollbar.log('Hello world!')
 const students = ['Jimmy', 'Timothy', 'Jimothy']
 
 app.get('/', (req, res) => {
+    rollbar.info("Someone loaded up your HTML!")
     res.sendFile(path.join(__dirname, '/index.html'))
 })
 
 app.get('/api/students', (req, res) => {
-    rollbar.info("Someone got the list of students to load")
+    rollbar.info("Someone got the list of students to load");
     res.status(200).send(students)
 })
 
@@ -35,12 +36,15 @@ app.post('/api/students', (req, res) => {
    })
 
    try {
+        rollbar.log("Student added successfully", {author: "Kevin", type: "manual entry"});
        if (index === -1 && name !== '') {
            students.push(name)
            res.status(200).send(students)
        } else if (name === ''){
+        rollbar.error("No name provided");
            res.status(400).send('You must enter a name.')
        } else {
+        rollbar.error("Student already exists");
            res.status(400).send('That student already exists.')
        }
    } catch (err) {
@@ -52,6 +56,7 @@ app.delete('/api/students/:index', (req, res) => {
     const targetIndex = +req.params.index
     
     students.splice(targetIndex, 1)
+    rollbar.info("Student was deleted");
     res.status(200).send(students)
 })
 
